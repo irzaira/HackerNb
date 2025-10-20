0d6f4fb8ac62f9b9741cdb221bf7676b1080e378
irzaira <irfhan10@gmail.com>
2025-10-20 22:16:45 +0900

    Add files via upload
    Done

---
+++ MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hackernb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Placeholder activity for HackerNb
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,1 @@
-plugins {
-    kotlin("jvm") version "1.9.0" apply false
-    id("com.android.application") version "8.1.0" apply false
-}
+// App build.gradle.kts placeholder
